41b2a46118c0ea38ee469b3d4175499111355428
karthickmce34 <karthickmce34@gmail.com>
2022-08-18 16:37:44 +0530

    commit for correction done on aug18_08_22

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -47,8 +47,8 @@ class ProjectController extends Controller
         
         $model = new $this->modelName();
         $record = $model->find($id);
-        echo "<pre>";
-        print_r($record->projectsummary->expense);die;
+        //echo "<pre>";
+        //print_r($record->projectsummary->expense);die;
         if(!$record)
         {
             return redirect()->route($this->baseRedirect);    
